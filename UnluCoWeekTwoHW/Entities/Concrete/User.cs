6815322d707e6f85067fa7f5dd5393e29a114de4
CaglarMacha <caglar.ayhan@boun.edu.tr>
2022-01-26 14:45:45 +0300

    Deconstruct methods was added

--- UnluCoWeekTwoHW/Entities/Concrete/User.cs
+++ UnluCoWeekTwoHW/Entities/Concrete/User.cs
@@ -12,5 +12,11 @@ namespace UnluCoWeekTwoHW.Entities.Concrete
         public string SecondName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+
+        public void Deconstruct (out string _userName, out string _password)
+        {
+            _userName = UserName;
+            _password = Password;
+        }
     }
 }
