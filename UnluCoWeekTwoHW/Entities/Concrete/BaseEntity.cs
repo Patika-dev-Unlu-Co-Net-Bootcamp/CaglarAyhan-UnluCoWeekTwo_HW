52a80d18bd9b22e249485fee5dd49618e124bf49
CaglarMacha <caglar.ayhan@boun.edu.tr>
2022-01-26 20:37:11 +0300

    base entity was added

---
+++ UnluCoWeekTwoHW/Entities/Concrete/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnluCoWeekTwoHW.Entities.Abstract;
+
+namespace UnluCoWeekTwoHW.Entities.Concrete
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}
